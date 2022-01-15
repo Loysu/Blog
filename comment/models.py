25d3ab7020334bde85d3886cd865e4c058efebc7
Pavel <prohorenkopavel74@gmail.com>
2022-01-15 13:28:53 +0500

    add comment model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, verbose_name='Автор комментария', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    text = models.TextField(verbose_name='Текст комментария')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return f"{self.id}"
+
+    def save(self, *args, **kwargs):
+        if self.author.profile:
+            self.author = self.author.profile
+        super().save(*args, **kwargs)
